c5befcff55814009090893aaa21f2773a40aa6e3
Ethan Anderson <ethan.anderson00@gmail.com>
2020-01-25 16:53:41 +0000

    Added more projects to home page

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -50,8 +50,16 @@ export class AppHome {
             <p>An NPM package I created that allows for the converting of conventional text into alternating caps</p>            
           </div>
           <div class='project'>
+            <a target='_blank' rel='noopener noreferrer' href='https://github.com/ZaphodAndo/scuffed-text-cli'>Scuffed Text CLI</a>
+            <p>A CLI tool for converting text into alternating caps.</p>            
+          </div>
+          <div class='project'>
             <a target='_blank' rel='noopener noreferrer' href='https://github.com/kanaikimi/project-fanta'>Project Fanta</a>
             <p>Project Fanta is an instant and simple platform for anybody who wants to broadcast messages to subscribers using SMS!</p>            
+          </div>
+          <div class='project'>
+            <a target='_blank' rel='noopener noreferrer' href='https://github.com/ZaphodAndo/save-my-eyes'>Save My Eyes</a>
+            <p>The VS Code theme that doesn't burn code into your eyes!</p>            
           </div>
         </div>
       </div>
